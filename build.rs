551b4e5e0b452ae09a2c87f338af6b0b65da8dd5
anilpandey071999 <anilpandey071999@gmail.com>
2025-06-08 19:24:31 +0530

    Add rustls with default provider and refactor QUIC server
    The changes make the QUIC server fully functional by initializing rustls with
    the Ring crypto provider and improving connection handling.

--- build.rs
+++ build.rs
@@ -1,11 +1,15 @@
+use rcgen::CertifiedKey;
 use std::fs;
 use std::path::Path;
-use rcgen::CertifiedKey;
 
 fn main() {
+    // if Path::exists("&self")
     // `.der` is binary file for the certificate
-    if !Path::new("cert.der").exists(){
-        let CertifiedKey{cert, key_pair} = rcgen::generate_simple_self_signed(vec!["localhost".into()]).expect("Failed to create cert!!");
+    if !Path::new("cert.der").exists() {
+        println!("Certificate not found, generating new one...");
+        let CertifiedKey { cert, key_pair } =
+            rcgen::generate_simple_self_signed(vec!["localhost".into()])
+                .expect("Failed to create cert!!");
         fs::write("key.der", key_pair.serialize_der()).expect("Failed to write key");
         fs::write("cert.der", cert.der()).expect("Failed to write cert");
     }
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,50 @@
+use anyhow::Result;
 use quinn::rustls::pki_types::PrivateKeyDer;
 use quinn::{Endpoint, ServerConfig};
 use std::fs;
 use std::net::SocketAddr;
 use std::sync::Arc;
-use anyhow::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    rustls::crypto::ring::default_provider()
+        .install_default()
+        .unwrap();
+
     let address: SocketAddr = "127.0.0.1:4433".parse().unwrap();
-    
+
     let cert = fs::read("cert.der").unwrap();
     let key = fs::read("key.der").unwrap();
-    let server_config = ServerConfig::with_single_cert(vec![cert.into()], PrivateKeyDer::try_from(key).unwrap())?;
+    let server_config =
+        ServerConfig::with_single_cert(vec![cert.into()], PrivateKeyDer::try_from(key).unwrap())?;
     let endpoint = Endpoint::server(server_config, address)?;
     println!("Listening on {}", endpoint.local_addr()?);
-    
-    while let Some(conn) = endpoint.accept().await{
+
+    while let Some(conn) = endpoint.accept().await {
         println!("Connection incoming from: {}", conn.remote_address());
         tokio::spawn(async move {
-            let conn = conn.accept().unwrap();
             handle_connection(conn).await;
         });
     }
     Ok(())
 }
 
-async fn handle_connection(conn: quinn::Connecting) {
+async fn handle_connection(conn: quinn::Incoming) {
     // Handle the connection here
     match conn.await {
         Ok(data) => {
-            while let (mut send, mut recv) = data.accept_bi().await.unwrap(){
+            println!("Connection established for {}!", data.remote_address());
+            while let Ok((mut send, mut recv)) = data.accept_bi().await {
                 let msg = recv.read_to_end(1024).await.unwrap_or_default();
                 let msg_str = String::from_utf8_lossy(&msg);
                 println!("Recevied: {}", msg_str);
-                
+
                 send.write_all(&msg).await.unwrap();
                 println!("Echoed message back.");
             }
-        },
+        }
         Err(e) => {
             eprintln!("Failed to establish connection: {}", e)
-        },
+        }
     }
 }
